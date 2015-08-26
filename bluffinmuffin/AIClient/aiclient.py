fb34db9f0431b8adc073426df767e25ab063d0a7
Mathieu Germain <mathieu.germain2@usherbrooke.ca>
2015-08-26 00:18:37 -0400

    Added identification to the lobby.

--- bluffinmuffin/AIClient/aiclient.py
+++ bluffinmuffin/AIClient/aiclient.py
@@ -6,6 +6,7 @@ from bluffinmuffin import protocol as proto
 class AIClient(object):
 
     def __init__(self, ai_type, server, port):
+        self.ai_type = ai_type
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._connect(server, port)
 
@@ -14,13 +15,26 @@ class AIClient(object):
         self.socket.connect((server, port))
         print("Done")
 
+        # Check version
         self._send(proto.lobby.CheckCompatibilityCommand({"ImplementedProtocolVersion": "2.2.0"}).encode())
         compat_rep = self._receive()
-
         if compat_rep['Success']:
             print("The server is compatible!")
         else:
             raise Exception("Server incompatible!!")
+
+        # Identify
+        ai_name = "AI-{}".format(self.ai_type)
+        ai_id = 1
+        ident_rep = {"Success": False}
+        while not ident_rep['Success']:
+            self._send(proto.lobby.quick_mode.IdentifyCommand({"Name": ai_name}).encode())
+            ident_rep = self._receive()
+            if not ident_rep['Success']:
+                print(ident_rep['Message'] + " Trying a different one.")
+                ai_name += str(ai_id)
+                ai_id += 1
+        print("Logged in as: {}".format(ai_name))
 
     def _send(self, msg):
         msg = "{}\n".format(msg).encode('ascii')
@@ -32,9 +46,10 @@ class AIClient(object):
         rep = b""
         while not rep.endswith(b'\n'):
             rep += self.socket.recv(100)
-        return json.loads(rep.decode("utf-8") )
+        return json.loads(rep.decode("utf-8"))
 
     def __del__(self):
         self._send(proto.DisconnectCommand({}).encode())
+        print("Logged Out!")
         self.socket.shutdown(1)
         self.socket.close()
